1b2ff765ffaed6ff41b7ebc31c1250ae8dab4478
Peter McQuillan <beatofthedrum@gmail.com>
2022-08-08 19:09:23 +0100

    Added support for Firefox. Added benchmark test.
    Previous version used API which was not supported by Firefox browser, new version should work correctly on Firefox.
    Also added very basic test page so users can see how long it takes to decode a file via WebAssembly.

--- mywavpack_timer.js
+++ mywavpack_timer.js
@@ -40,7 +40,6 @@ fileElem.addEventListener("change", handleFiles, false);
   let file = this.files[0];
   let data; 
 
-
   var Reader = new FileReader();
   Reader.readAsArrayBuffer(file);
   Reader.onload = async function () {
@@ -52,10 +51,9 @@ fileElem.addEventListener("change", handleFiles, false);
      FS.write(stream, array, 0, array.length, 0);
      FS.close(stream);
 
-     fileElem.value = null; // so we can reload the same file is needed (otherwise change event may not be triggered)
+     fileElem.value = null; // so we can reload the same file is needed (otherwise change event may not be trigerred)
    
      playbackStatus = 0;
-     decodedsamples = 0;
    
      const bytes_per_element = Module.HEAP32.BYTES_PER_ELEMENT;
    
@@ -86,63 +84,19 @@ fileElem.addEventListener("change", handleFiles, false);
      console.log("bytes per sample is ", bps);
    
      floatDivisor = Math.pow(2, bps * 8 - 1);
+
+     var mytextarea = document.getElementById('mytextarea');
+     mytextarea.innerHTML = "<br />";
+
+     var startTime = performance.now();
+
+     decodedamount = 1; // just so we call our main loop at least once
+     decodedsamples = 0;
+
+     while(decodedamount>0)
+     {
    
-     context = new AudioContext({
-       latencyHint: "interactive",
-       sampleRate: sample_rate,
-     });
-   
-     oninput = handleVolumeEvents;
-   
-     //var aw = await context.audioWorklet.addModule("./messenger-processor.js");
-   
-     await fetch("messenger-processor.js")
-       .then((response) => response.text())
-       .then((text) => {
-           const blob = new Blob([text], { type: 'application/javascript; charset=utf-8' });
-           const objectUrl = URL.createObjectURL(blob);
-   
-           return context.audioWorklet.addModule(objectUrl)
-               .finally(() => URL.revokeObjectURL(objectUrl));
-       })
-   
-     
-     var awopt = { outputChannelCount: [numChannels] }; // so we can get stereo output, this is our AudioWorkletNodeOptions
-     messengerWorkletNode = new MessengerWorkletNode(context, awopt);
-     messengerWorkletNode.connect(context.destination);
-  };
-
-  
-}
-
-
-function handleVolumeEvents(evt) {
-  const value = evt.target.value;
-  volume = +value;
-}
-
-butStop.addEventListener("click", async () => {
-  playbackStatus = 2; // we have been requested to stop
-});
-
-// Extends AudioWorkletNode to simplify the cross-thread message posting.
-class MessengerWorkletNode extends AudioWorkletNode {
-  constructor(context, awopt) {
-    super(context, "messenger-processor", awopt);
-    this.counter_ = 0;
-    this.port.onmessage = this.handleMessage_.bind(this);
-  }
-
-  handleMessage_(event) {
-    if(isNaN(volume))
-    {
-      volume = 1;
-    }
-    messengerWorkletNode.parameters.get("customVolumeAdjust").value =
-      Math.pow(2, bps * 8 - 1) / volume;
-
-
-    decodedamount = Module.ccall(
+     decodedamount = Module.ccall(
       "DecodeWavPackBlock",
       "number",
       ["number", "number", "number"],
@@ -151,27 +105,18 @@ class MessengerWorkletNode extends AudioWorkletNode {
 
     let output_array = new Int32Array(Module.HEAP32.buffer, arrayPointer, 4096);
     decodedsamples = decodedsamples + decodedamount;
+     }
 
-    if (decodedamount == 0) {
-      // zero out to bytes just to be sure
-      for (let i = 0; i < 2048; i++) {
-        output_array[i] = 1;
-      } 
-      playbackStatus = 1;
-    }
+     var endTime = performance.now();
 
-    // if decoded amount is less than 1024 (and not zero), we fill array with silence/zero
-    if (decodedamount < 1024 && decodedamount > 0) {
-      for (let i = decodedamount * numChannels; i < 1024 * numChannels; i++) {
-        output_array[i] = 1;
-      }
-      playbackStatus = 1;
-    }
+     console.log("It took " + (endTime-startTime) + " milliseconds to decode the file");
+     console.log("We decoded " + decodedsamples + " samples");
 
-    // We use 2048 as we decode 1024 samples and data may be stereo i.e. 2x1024
-    const segment = output_array.slice(0, 2049); // 2048 plus 1 for status
-    segment[2048] = playbackStatus;
+     mytextarea.innerHTML += "It took " + (endTime-startTime) + " milliseconds to decode the file<br />";
+     mytextarea.innerHTML += "We decoded " + decodedsamples + " samples"
+  };
 
-    this.port.postMessage(segment, [segment.buffer]);
-  }
+  
 }
+
+
